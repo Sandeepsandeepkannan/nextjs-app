4a09efa13a4f6332e1e084d3fee800480e864d5b
Sandeep <sandeepsandeepkannan16@gmail.com>
2025-12-23 11:40:05 +0530

    first commit

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function signinpage(){
+  return <div className="bg-gray-600 h-screen pl-150 pt-60"> 
+        <div className="bg-white w-50">
+                <input type="text"  placeholder="name"/>
+                <br />
+                <input type="text"  placeholder="password"/>
+        </div>
+         
+               <button className="bg-white">sigin</button>
+     
+    </div>
+}
